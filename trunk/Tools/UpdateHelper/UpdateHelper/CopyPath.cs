573832a9f864299caa6c1ea8b130e0b396189fc5
taisandog64@vip.qq.com <taisandog64@vip.qq.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2012-03-24 17:09:49 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@96 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/Tools/UpdateHelper/UpdateHelper/CopyPath.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Windows.Forms;
+
+namespace UpdateHelper
+{
+    public class CopyPath
+    {
+        private string _sourcePath;
+        /// <summary>
+        /// 源目录
+        /// </summary>
+        public string SourcePath
+        {
+            get { return _sourcePath; }
+            set { _sourcePath = value; }
+        }
+
+        private string _targetPath;
+        /// <summary>
+        /// 目标目录
+        /// </summary>
+        public string TargetPath
+        {
+            get { return _targetPath; }
+            set { _targetPath = value; }
+        }
+
+        private static string[] _allowFile ={".dll",".xml"};
+
+        /// <summary>
+        /// 拷贝
+        /// </summary>
+        /// <returns></returns>
+        public int DoCopy() 
+        {
+            string sPath=_basePath+"\\"+SourcePath;
+            string tPath = _basePath + "\\" + TargetPath;
+            if (!Directory.Exists(tPath)) 
+            {
+                Directory.CreateDirectory(tPath);
+            }
+            sPath=sPath.TrimEnd('\\');
+            tPath= tPath.TrimEnd('\\');
+            string[] files= Directory.GetFiles(sPath);
+            int totle = 0;
+            foreach (string file in files) 
+            {
+                FileInfo fInfo = new FileInfo(file);
+                if (!fInfo.Exists) 
+                {
+                    continue;
+                }
+
+                string extension = fInfo.Extension;
+                bool isAllow = false;
+                foreach (string allow in _allowFile) 
+                {
+                    if (extension.Equals(allow,StringComparison.CurrentCultureIgnoreCase)) 
+                    {
+                        isAllow = true;
+                        break;
+                    }
+
+                }
+
+                if (!isAllow) 
+                {
+                    continue;
+                }
+                try
+                {
+                    File.Copy(file, tPath + "\\" + fInfo.Name, true);
+                }
+                catch(Exception ex) 
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                totle++;
+            }
+            return totle;
+        }
+        static string _basePath = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+
+        /// <summary>
+        /// 获取需要拷贝的路径
+        /// </summary>
+        /// <returns></returns>
+        public static List<CopyPath> GetPath() 
+        {
+            string infoFile = _basePath + "\\UpdateConfig.xml";
+            if (!File.Exists(infoFile))
+            {
+                return null;
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(infoFile);
+            XmlNodeList addinNodes = doc.GetElementsByTagName("Path");
+            List<CopyPath> lstAddIn = new List<CopyPath>();
+            foreach (XmlNode node in addinNodes)
+            {
+                CopyPath info = new CopyPath();
+                XmlAttribute att = node.Attributes["source"];
+                if (att != null)
+                {
+                    info._sourcePath = att.InnerText;
+                }
+                att = node.Attributes["target"];
+                if (att != null)
+                {
+                    info._targetPath = att.InnerText;
+                }
+                lstAddIn.Add(info);
+            }
+            return lstAddIn;
+        }
+    }
+}
