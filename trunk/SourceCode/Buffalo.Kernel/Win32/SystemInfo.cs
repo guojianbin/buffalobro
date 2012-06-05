99cdc9480b99d5bab842dcaedab4ea886ce8c623
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2012-06-05 05:33:53 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@133 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/SourceCode/Buffalo.Kernel/Win32/SystemInfo.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Buffalo.Kernel.Win32
+{
+    /// <summary>
+    /// 系统信息
+    /// </summary>
+    public class SystemInfo
+    {
+        public delegate bool DelIsWow64Process(IntPtr pHandle, ref bool is64);
+        /// <summary>
+        /// 是否64位系统
+        /// </summary>
+        /// <returns></returns>
+        public static bool Is64System()
+        {
+            //bool ret = false;
+            //IntPtr pModule = WindowsAPI.GetModuleHandle("kernel32.dll");
+            //IntPtr ptrPro = WindowsAPI.GetProcAddress(pModule, "IsWow64Process");
+
+            //DelIsWow64Process delIs64 = Marshal.GetDelegateForFunctionPointer(ptrPro, typeof(DelIsWow64Process)) as DelIsWow64Process;
+            //if (delIs64 != null)
+            //{
+                
+            //    IntPtr proPtr = Process.GetCurrentProcess().Handle;
+            //    delIs64(proPtr, ref ret);
+            //}
+            //return ret;
+
+            return IntPtr.Size == 8;
+        }
+
+        /// <summary>
+        /// Gets a value indicating if the operating system is a Windows 2000 or a newer one.
+        /// </summary>
+        public static bool IsWindows2000OrNewer
+        {
+            get { return (Environment.OSVersion.Platform == PlatformID.Win32NT) && (Environment.OSVersion.Version.Major >= 5); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating if the operating system is a Windows XP or a newer one.
+        /// </summary>
+        public static bool IsWindowsXpOrNewer
+        {
+            get
+            {
+                return
+                    (Environment.OSVersion.Platform == PlatformID.Win32NT) &&
+                    (
+                        (Environment.OSVersion.Version.Major >= 6) ||
+                        (
+                            (Environment.OSVersion.Version.Major == 5) &&
+                            (Environment.OSVersion.Version.Minor >= 1)
+                        )
+                    );
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating if the operating system is a Windows Vista or a newer one.
+        /// </summary>
+        public static bool IsWindowsVistaOrNewer
+        {
+            get { return (Environment.OSVersion.Platform == PlatformID.Win32NT) && (Environment.OSVersion.Version.Major >= 6); }
+        }
+    }
+}
