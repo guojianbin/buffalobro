1fc8f3e8ab5486f84611d3a854ebd5c8bcf1c9a9
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2012-10-08 09:39:47 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@187 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/ModelCompiler/ModelCompiler/Compiler.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ModelCompiler
+{
+    public class Compiler
+    {
+        private string _content;
+        private int index = 0;
+        
+
+        public Compiler(string content) 
+        {
+            
+        }
+
+        public string GetContent() 
+        {
+            
+        }
+
+    }
+}
