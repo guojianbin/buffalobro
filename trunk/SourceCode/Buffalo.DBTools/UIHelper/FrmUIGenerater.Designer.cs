5b631350a915d960a81193f7cc67c51868c939af
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-01-09 10:08:35 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@233 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

--- trunk/SourceCode/Buffalo.DBTools/UIHelper/FrmUIGenerater.Designer.cs
+++ trunk/SourceCode/Buffalo.DBTools/UIHelper/FrmUIGenerater.Designer.cs
@@ -31,23 +31,82 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUIGenerater));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.gvMember = new System.Windows.Forms.DataGridView();
+            this.ColCheck = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMember)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 426);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(688, 63);
+            this.panel1.Size = new System.Drawing.Size(688, 71);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 63);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(688, 434);
+            this.panel2.Size = new System.Drawing.Size(688, 426);
             this.panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(184, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(504, 426);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.gvMember);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(184, 426);
+            this.panel3.TabIndex = 0;
+            // 
+            // gvMember
+            // 
+            this.gvMember.AllowUserToAddRows = false;
+            this.gvMember.AllowUserToDeleteRows = false;
+            this.gvMember.AllowUserToResizeColumns = false;
+            this.gvMember.AllowUserToResizeRows = false;
+            this.gvMember.BackgroundColor = System.Drawing.Color.White;
+            this.gvMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvMember.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColCheck,
+            this.ColName});
+            this.gvMember.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvMember.Location = new System.Drawing.Point(0, 0);
+            this.gvMember.Name = "gvMember";
+            this.gvMember.RowHeadersVisible = false;
+            this.gvMember.RowTemplate.Height = 23;
+            this.gvMember.Size = new System.Drawing.Size(184, 426);
+            this.gvMember.TabIndex = 0;
+            // 
+            // ColCheck
+            // 
+            this.ColCheck.HeaderText = "";
+            this.ColCheck.Name = "ColCheck";
+            this.ColCheck.Width = 50;
+            // 
+            // ColName
+            // 
+            this.ColName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColName.HeaderText = "属性";
+            this.ColName.Name = "ColName";
+            this.ColName.ReadOnly = true;
             // 
             // FrmUIGenerater
             // 
@@ -59,6 +118,9 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmUIGenerater";
             this.Text = "界面生成";
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gvMember)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,5 +129,11 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView gvMember;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColCheck;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColName;
+
     }
 }
---
+++ trunk/SourceCode/Buffalo.WinFormsControl/Editors/TextBoxEditor.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Buffalo.WinFormsControl.Editors
+{
+    /// <summary>
+    /// 文本框的编辑器
+    /// </summary>
+    public partial class TextBoxEditor : UserControl
+    {
+        public TextBoxEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxEditor_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pnlValue_Resize(object sender, EventArgs e)
+        {
+            int width = pnlValue.Width - 4;
+            if (width > 0) 
+            {
+                txtValue.Width = width;
+            }
+        }
+        public override string  Text
+        {
+	        get 
+	        { 
+		         return txtValue.Text;
+	        }
+	          set 
+	        { 
+		        txtValue.Text = value;
+	        }
+        }
+        /// <summary>
+        /// 标签宽度
+        /// </summary>
+        public int LableWidth 
+        {
+            get 
+            {
+                return pnlLable.Width;
+            }
+            set 
+            {
+                pnlLable.Width = value;
+            }
+        }
+
+        /// <summary>
+        /// 标签字体
+        /// </summary>
+        public Font LableFont
+        {
+            get
+            {
+                return labSummary.Font;
+            }
+            set
+            {
+                labSummary.Font=value;
+            }
+        }
+
+        /// <summary>
+        /// 标签内容
+        /// </summary>
+        public string LableText
+        {
+            get 
+            {
+                return labSummary.Text;
+            }
+            set 
+            {
+                labSummary.Text = value;
+            }
+        }
+
+
+    }
+}
