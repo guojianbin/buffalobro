883017fec5422ea4523f1b5236af2df68a59a3d1
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-01-29 10:13:01 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@253 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

--- trunk/SourceCode/Buffalo.DBTools/UIHelper/FrmUIGenerater.cs
+++ trunk/SourceCode/Buffalo.DBTools/UIHelper/FrmUIGenerater.cs
@@ -35,7 +35,7 @@ namespace Buffalo.DBTools.UIHelper
 
         //private object _currentEntity;
 
-       
+        private UIModelItem _classInfo = null;
 
         private UIConfigItem _config;
         /// <summary>
@@ -82,14 +82,24 @@ namespace Buffalo.DBTools.UIHelper
             pnlClassConfig.Controls.Clear();
             for (int i = 0; i < lstItem.Count; i++)
             {
-                Control ctr = NewItem(lstItem[i]);
+                EditorBase editor = NewItem(lstItem[i]);
 
-                pnlClassConfig.Controls.Add(ctr);
-                tabPanel.Controls.SetChildIndex(ctr, i);
-
+                pnlClassConfig.Controls.Add(editor);
+                tabPanel.Controls.SetChildIndex(editor, i);
+                editor.OnValueChange += new ValueChangeHandle(editorClass_OnValueChange);
             }
         }
+        void editorClass_OnValueChange(object sender, object oldValue, object newValue)
+        {
+            EditorBase editor = sender as EditorBase;
+            if (editor == null)
+            {
+                return;
+            }
+            
+            _classInfo.CheckItem[editor.BindPropertyName] = editor.Value;
 
+        }
         /// <summary>
         /// 创建控件
         /// </summary>
@@ -103,13 +113,13 @@ namespace Buffalo.DBTools.UIHelper
             AddCellStyle();
             for (int i = 0; i < lstItem.Count; i++) 
             {
-                Control ctr = NewItem(lstItem[i]);
+                EditorBase editor = NewItem(lstItem[i]);
                 int col = i % 2;
                 int row = i / 2;
-                tabPanel.Controls.Add(ctr, col, row);
-                tabPanel.Controls.SetChildIndex(ctr, i);
-                
-                ctr.Dock = DockStyle.Left;
+                tabPanel.Controls.Add(editor, col, row);
+                tabPanel.Controls.SetChildIndex(editor, i);
+                editor.OnValueChange += new ValueChangeHandle(editor_OnValueChange);
+                editor.Dock = DockStyle.Left;
                 
             }
         }
@@ -127,7 +137,7 @@ namespace Buffalo.DBTools.UIHelper
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
-        private Control NewItem(ConfigItem item) 
+        private EditorBase NewItem(ConfigItem item) 
         {
             EditorBase editor = item.GetEditor();
             
@@ -136,7 +146,7 @@ namespace Buffalo.DBTools.UIHelper
             editor.LableFont = new Font(editor.LableFont.FontFamily, 9, FontStyle.Bold);
             editor.LableWidth=80;
             editor.Width = 230;
-            editor.OnValueChange += new ValueChangeHandle(editor_OnValueChange);
+            
             return editor;
         }
 
@@ -208,6 +218,8 @@ namespace Buffalo.DBTools.UIHelper
             CreateItems();
             CreateClassItem();
             this.Text = "UI界面生成-" + _curEntityInfo.ClassName;
+
+            _classInfo = new UIModelItem();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -229,13 +241,22 @@ namespace Buffalo.DBTools.UIHelper
 
         private void btnGen_Click(object sender, EventArgs e)
         {
+            SaveItemInfos();
+            SaveClassItemInfo();
+        }
+
+        /// <summary>
+        /// 保存项选择信息
+        /// </summary>
+        private void SaveItemInfos() 
+        {
             XmlDocument doc = new XmlDocument();
-            XmlNode root=doc.CreateElement("root");
+            XmlNode root = doc.CreateElement("root");
             doc.AppendChild(root);
             List<UIModelItem> lst = gvMember.DataSource as List<UIModelItem>;
-            foreach (UIModelItem item in lst) 
+            foreach (UIModelItem item in lst)
             {
-                if (!item.IsGenerate) 
+                if (!item.IsGenerate)
                 {
                     continue;
                 }
@@ -243,12 +264,49 @@ namespace Buffalo.DBTools.UIHelper
                 root.AppendChild(inode);
                 item.WriteNode(inode);
             }
-            string directory=ModelPath+"gencache\\";
+            string directory = ModelPath + "gencache\\";
             if (!Directory.Exists(directory))
             {
                 Directory.CreateDirectory(directory);
             }
-            string fileName=directory+"\\"+_curEntityInfo.FullName+".cache.xml";
+            string fileName = directory + "\\" + _curEntityInfo.FullName + ".cache.xml";
+            doc.Save(fileName);
+        }
+
+        private void SaveClassItemInfo() 
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode root = doc.CreateElement("root");
+            doc.AppendChild(root);
+            Dictionary<string,object> div = _classInfo.CheckItem;
+            foreach (KeyValuePair<string, object> kvp in div)
+            {
+                XmlNode inode = doc.CreateElement("item");
+                XmlAttribute att = doc.CreateAttribute("name");
+                att.InnerText = kvp.Key;
+                inode.Attributes.Append(att);
+
+                string value = null;
+                if (kvp.Value is bool)
+                {
+                    value = (bool)kvp.Value ? "1" : "0";
+                }
+                else
+                {
+                    value = kvp.Value as string;
+                }
+                att = doc.CreateAttribute("value");
+                att.InnerText = value;
+                inode.Attributes.Append(att);
+                root.AppendChild(inode);
+            }
+
+            string directory = ModelPath + "\\";
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            string fileName = directory + "\\classinfo.cache.xml";
             doc.Save(fileName);
         }
 
