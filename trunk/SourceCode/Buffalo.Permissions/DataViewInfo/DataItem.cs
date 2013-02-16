ba61da3db942fbc4c9d5727c2928c21559326ab9
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-02-16 12:15:29 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@263 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/SourceCode/Buffalo.Permissions/DataViewInfo/DataItem.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Buffalo.DB.BQLCommon.BQLConditionCommon;
+
+namespace Buffalo.Permissions.DataViewInfo
+{
+    /// <summary>
+    /// 数据视图的数据项
+    /// </summary>
+    public class DataItem
+    {
+        private string _propertyName;
+        /// <summary>
+        /// 属性名
+        /// </summary>
+        public string PropertyName
+        {
+            get { return _propertyName; }
+        }
+        private Type _propertyType;
+        /// <summary>
+        /// 属性类型
+        /// </summary>
+        public Type PropertyType
+        {
+            get { return _propertyType; }
+        }
+        private bool _canView;
+        /// <summary>
+        /// 是否能查看
+        /// </summary>
+        public bool CanView
+        {
+            get { return _canView; }
+            set { _canView=value; }
+        }
+        private bool _canAdd;
+        /// <summary>
+        /// 是否能添加
+        /// </summary>
+        public bool CanAdd
+        {
+            get { return _canAdd; }
+            set { _canAdd = value; }
+        }
+        private bool _canEdit;
+        /// <summary>
+        /// 是否能编辑
+        /// </summary>
+        public bool CanEdit
+        {
+            get { return _canEdit; }
+            set { _canEdit = value; }
+        }
+        private SumType _sumType;
+        /// <summary>
+        /// 统计类型
+        /// </summary>
+        public SumType SumType
+        {
+            get { return _sumType; }
+            set { _sumType = value; }
+        }
+        private BQLParamHandle _customSum;
+        /// <summary>
+        /// 自定义统计类型(SumType=SumType.Custom)时候的统计表达式
+        /// </summary>
+        public BQLParamHandle CustomSum
+        {
+            get { return _customSum; }
+            set { _customSum = value; }
+        }
+
+        /// <summary>
+        /// 数据视图的数据项
+        /// </summary>
+        /// <param name="propertyName">属性名</param>
+        /// <param name="propertyType">属性类型</param>
+        /// <param name="canView">能查看</param>
+        /// <param name="canAdd">能添加</param>
+        /// <param name="canEdit">能编辑</param>
+        /// <param name="sumType">统计类型</param>
+        /// <param name="customSum">自定义统计项</param>
+        public DataItem(string propertyName, Type propertyType, bool canView, bool canAdd, bool canEdit, SumType sumType, BQLParamHandle customSum) 
+            :this(propertyName,propertyType)
+        {
+            _canAdd = canAdd;
+            _canEdit = canEdit;
+            _canView = canView;
+            _customSum = customSum;
+            _sumType = sumType;
+        }
+
+        /// <summary>
+        /// 数据视图的数据项
+        /// </summary>
+        /// <param name="propertyName">属性名</param>
+        /// <param name="propertyType">属性类型</param>
+        public DataItem(string propertyName,Type propertyType) 
+        {
+            _propertyName = propertyName;
+            _propertyType = propertyType;
+        }
+    }
+
+    /// <summary>
+    /// 统计类型
+    /// </summary>
+    public enum SumType 
+    {
+        None=1,
+        Sum=2,
+        Count=3,
+        Avg=4,
+        Custom=5
+    }
+}
---
+++ trunk/SourceCode/Buffalo.Permissions/DataViewInfo/DataViewer.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Buffalo.DB.BQLCommon.BQLConditionCommon;
+
+namespace Buffalo.Permissions.DataViewInfo
+{
+    /// <summary>
+    /// 数据视图
+    /// </summary>
+    public class DataViewer:IEnumerable<DataItem>
+    {
+        private List<DataItem> _lstDataItem = new List<DataItem>();
+        private BQLEntityTableHandle _entityHandle;
+        /// <summary>
+        /// 实体信息
+        /// </summary>
+        public BQLEntityTableHandle EntityHandle
+        {
+            get { return _entityHandle; }
+        }
+
+        /// <summary>
+        /// 数据视图
+        /// </summary>
+        /// <param name="entityHandle">所属的实体信息</param>
+        public DataViewer(BQLEntityTableHandle entityHandle) 
+        {
+            _entityHandle = entityHandle;
+        }
+        /// <summary>
+        /// 创建视图数据项
+        /// </summary>
+        /// <param name="propertyName">属性名</param>
+        /// <param name="canView">能查看</param>
+        /// <param name="canAdd">能添加</param>
+        /// <param name="canEdit">能编辑</param>
+        /// <param name="sumType">统计类型</param>
+        /// <param name="customSum">自定义统计项</param>
+        /// <returns>数据项</returns>
+        protected DataItem CreateDataItem(string propertyName,string propertyType,
+            bool canView, bool canAdd, bool canEdit, 
+            SumType sumType, BQLParamHandle customSum) 
+        {
+            DataItem item = new DataItem(propertyName,propertyType, 
+                canView, canAdd, canEdit, sumType, customSum);
+            _lstDataItem.Add(item);
+            return item;
+        }
+
+        /// <summary>
+        /// 返回视图数据项
+        /// </summary>
+        /// <param name="index">索引</param>
+        /// <returns></returns>
+        public DataItem this[int index] 
+        {
+            get 
+            {
+                return _lstDataItem[index];
+            }
+        }
+
+        /// <summary>
+        /// 返回视图数据项
+        /// </summary>
+        /// <param name="propertyName">属性名</param>
+        /// <returns></returns>
+        public DataItem this[string propertyName]
+        {
+            get
+            {
+                int index = FindDataItemIndex(propertyName);
+                if (index > 0) 
+                {
+                    return _lstDataItem[index];
+                }
+                return null;
+            }
+        }
+
+        public int Count 
+        {
+            get 
+            {
+                return _lstDataItem.Count;
+            }
+        }
+
+        /// <summary>
+        /// 根据索引删除项
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public bool Remove(int index) 
+        {
+            if (index > _lstDataItem.Count - 1) 
+            {
+                return false;
+            }
+            
+            _lstDataItem.RemoveAt(index);
+            return true;
+        }
+        /// <summary>
+        /// 根据属性名删除项
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public bool Remove(string propertyName)
+        {
+
+            int index = FindDataItemIndex(propertyName);
+            if (index > 0)
+            {
+                _lstDataItem.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 查找视图项的索引
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        private int FindDataItemIndex(string propertyName) 
+        {
+            for (int i = 0; i < _lstDataItem.Count; i++) 
+            {
+                if (_lstDataItem[i].PropertyName == propertyName) 
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        #region IEnumerable<DataItem> 成员
+
+        public IEnumerator<DataItem> GetEnumerator()
+        {
+            return _lstDataItem.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable 成员
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _lstDataItem.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
