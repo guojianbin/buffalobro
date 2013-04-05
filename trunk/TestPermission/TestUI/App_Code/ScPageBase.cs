1413a7141d38a07611677484f6ab0219bf3ff7c6
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-04-05 11:30:11 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@295 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/TestPermission/TestUI/App_Code/ScPageBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using TestPerLib;
+using Buffalo.WebKernel.WebCommons.PageBases;
+
+/// <summary>
+/// ScPageBase 的摘要说明
+/// </summary>
+public class ScPageBase : PageBase<ScEmployee>
+{
+    public ScPageBase()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    /// <summary>
+    /// 获取数据的行数
+    /// </summary>
+    /// <param name="rowIndex"></param>
+    /// <param name="currentPage"></param>
+    /// <returns></returns>
+    public long GetDataIndex(int rowIndex, long currentPage)
+    {
+        return rowIndex + (PageSize * currentPage) + 1;
+    }
+    /// <summary>
+    /// 分页大小
+    /// </summary>
+    public virtual int PageSize 
+    {
+        get 
+        {
+            return 10;
+        }
+    }
+
+}
