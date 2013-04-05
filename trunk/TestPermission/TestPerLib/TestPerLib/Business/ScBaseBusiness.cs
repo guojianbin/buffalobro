14a3a0459fa0e1b80917e039a261805fa5a4077e
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-04-05 10:51:18 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@291 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/TestPermission/TestPerLib/TestPerLib/Business/ScBaseBusiness.cs
@@ -0,0 +1,57 @@
+using System;
+using TestPerLib;
+using System.Collections.Generic;
+using Buffalo.DB.DataBaseAdapter;
+using Buffalo.DB.CommBase.BusinessBases;
+using Buffalo.DB.CommBase.DataAccessBases;
+using Buffalo.DB.DbCommon;
+using Buffalo.DB.QueryConditions;
+using Buffalo.DB.CommBase;
+using Buffalo.DB.BQLCommon.BQLBaseFunction;
+namespace TestPerLib.Business
+{
+    /// <summary>
+    ///  业务层
+    /// </summary>
+    public class ScBaseBusinessBase<T>: BusinessModelBase<T> where T:ScBase,new()
+    {
+        //如果此实体需要被继承则在此写的业务方法能在子类的业务类中使用
+
+        public override object Insert(T entity, ValueSetList setList, bool fillIdentity)
+        {
+            if (setList == null) 
+            {
+                setList = new ValueSetList();
+            }
+            setList.Add("LastDate", BQL.NowDate());
+            setList.Add("CreateDate", BQL.NowDate());
+            return base.Insert(entity, setList, fillIdentity);
+        }
+
+        public override object Update(T entity, ScopeList scorpList, ValueSetList lstValue, bool optimisticConcurrency)
+        {
+            if (lstValue == null) 
+            {
+                lstValue = new ValueSetList();
+            }
+            lstValue.Add("LastDate", BQL.NowDate());
+            return base.Update(entity, scorpList, lstValue, optimisticConcurrency);
+        }
+        /// <summary>
+        /// 保存对象
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public object Save(T entity) 
+        {
+            if(entity.Id>0)
+            {
+                return Update(entity);
+            }
+            return Insert(entity, true);
+        }
+    }
+}
+
+
+
