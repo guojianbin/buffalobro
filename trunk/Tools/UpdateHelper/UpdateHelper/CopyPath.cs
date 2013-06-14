9a02048b3396acde109056c0a4d34633c306f3c8
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-06-14 09:00:15 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@338 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

--- trunk/Tools/UpdateHelper/UpdateHelper/CopyPath.cs
+++ trunk/Tools/UpdateHelper/UpdateHelper/CopyPath.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Xml;
 using System.IO;
 using System.Windows.Forms;
+using Buffalo.Kernel;
 
 namespace UpdateHelper
 {
@@ -73,7 +74,7 @@ namespace UpdateHelper
                 }
                 try
                 {
-                    File.Copy(file, tPath + "\\" + fInfo.Name, true);
+                    CommonMethods.CopyNewer(file,tPath + "\\" + fInfo.Name);
                 }
                 catch(Exception ex) 
                 {
