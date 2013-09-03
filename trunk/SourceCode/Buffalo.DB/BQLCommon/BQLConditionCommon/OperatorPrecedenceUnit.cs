d435396338a457f196ee884f11197f6a662d2270
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-09-03 10:12:09 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@355 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

--- trunk/SourceCode/Buffalo.DB/BQLCommon/BQLConditionCommon/OperatorPrecedenceUnit.cs
+++ trunk/SourceCode/Buffalo.DB/BQLCommon/BQLConditionCommon/OperatorPrecedenceUnit.cs
@@ -9,6 +9,34 @@ namespace Buffalo.DB.BQLCommon.BQLConditionCommon
     /// </summary>
     public class OperatorPrecedenceUnit
     {
+        /// <summary>
+        /// 左边的数值是否需要加括号
+        /// </summary>
+        /// <param name="left">左边数值</param>
+        /// <param name="operLevel">运算符优先级</param>
+        /// <returns></returns>
+        public static bool LeftNeedBreak(IOperatorPriorityLevel left,int operLevel) 
+        {
+            if (left.PriorityLevel < operLevel) 
+            {
+                return true;
+            }
+            return false;
+        }
 
+        /// <summary>
+        /// 右边的数值是否需要加括号
+        /// </summary>
+        /// <param name="right">右边数值</param>
+        /// <param name="operLevel">运算符优先级</param>
+        /// <returns></returns>
+        public static bool RightNeedBreak(IOperatorPriorityLevel right, int operLevel)
+        {
+            if (right.PriorityLevel <= operLevel)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
