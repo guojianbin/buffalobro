8cd32443bb70dec2ce5ad264be4e7e4fa47a42a5
taisandog@gmail.com <taisandog@gmail.com@7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc>
2013-10-24 10:10:44 +0000

    git-svn-id: http://buffalobro.googlecode.com/svn@363 7a4da7e7-5410-2bd0-76f2-bf0dbb7f3bbc

---
+++ trunk/SourceCode/Buffalo.DB/CacheManager/ICacheAdaper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Buffalo.DB.CacheManager
+{
+    public interface ICacheAdaper
+    {
+        System.Data.DataSet GetData(string sql);
+        void RemoveBySQL(string sql);
+        void RemoveByTableName(string tableName);
+        bool SetData(System.Collections.Generic.ICollection<string> tableNames, string sql, System.Data.DataSet ds);
+    }
+}
